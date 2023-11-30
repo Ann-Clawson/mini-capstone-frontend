9f16738c6535a4256acb00e68652cdaace5af6e7
Ann Clawson <annmasonclawson@gmail.com>
2023-11-30 12:34:29 -0800

    renders images with mapping of images table thru products table

--- src/Content.jsx
+++ src/Content.jsx
@@ -10,7 +10,7 @@ export function Content() {
   const handleIndexProducts = () => {
     // console.log("handleIndexProducts");
     axios.get("http://localhost:3000/products.json").then((response) => {
-      console.log(response.data);
+      // console.log(response.data);
       setProducts(response.data);
     });
   };
--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,14 +1,21 @@
 /* eslint-disable react/prop-types */
 export function ProductsIndex(props) {
   return (
-    <h1>
+    <div>
       {props.products.map((product) => (
         <div key={product.id}>
+          <div>
+            {product.images.map((image) => (
+              <div key={image.id}>
+                <img src={image.url} width="200px" />
+              </div>
+            ))}
+          </div>
           <p>{product.name}</p>
           <p>{product.description}</p>
           <p>{product.price}</p>
         </div>
       ))}
-    </h1>
+    </div>
   );
 }
